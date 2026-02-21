c794a268f57996b409c49f54d2027e79355278b3
Gregorio Santos <gregoriohd@gmail.com>
2026-02-21 18:48:58 -0300

    initial commit

---
+++ src/test/java/br/com/gregoriosantos/PdvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gregoriosantos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
